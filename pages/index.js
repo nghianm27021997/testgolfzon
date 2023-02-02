9f425af6fdd41482d85ce92a3855cd9e9146bad3
etysonnguyen <e.tyson.nguyen@wesang.com>
2023-02-02 10:48:02 +0700

    First commit

--- pages/index.js
+++ pages/index.js
@@ -1,115 +1,80 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import { Input, Skeleton, Table } from "antd";
+import { useState, useCallback, useEffect, useMemo } from "react";
+import { debounce, isEmpty } from "lodash";
+import axios from "axios";
 
 export default function Home() {
+  const [products, setProducts] = useState([]);
+  const [searchParams, setSearchParams] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  //get all products with limit
+  const fetchData = (key) => {
+    setLoading(true)
+    let url = `https://dummyjson.com/products/?limit=10&skip=5&q=${searchParams}`;
+    axios
+      .get(url)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err))
+      .finally(() => setLoading(false));
+  };
+
+  const debounceDropDown = useCallback(
+    debounce((nextValue) => fetchData(nextValue), 1500),
+    []
+  );
+
+  const handleInputOnchange = (e) => {
+    const { value } = e.target;
+    setSearchParams(value);
+    debounceDropDown(value);
+  };
+
+  useEffect(() => {
+    fetchData(searchParams);
+  }, []);
+
+  const columns = useMemo(() => [
+    {
+      title: "Brand",
+      dataIndex: "brand",
+      key: "brand",
+    },
+    {
+      title: "Title",
+      dataIndex: "title",
+      key: "title",
+    },
+    {
+      title: "Category",
+      dataIndex: "category",
+      key: "category",
+    },
+    {
+      title: "Price",
+      dataIndex: "price",
+      key: "price",
+    },
+    {
+      title: "Rating",
+      dataIndex: "rating",
+      key: "rating",
+    },
+  ]);
+
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <Input placeholder="Search Products" onKeyUp={handleInputOnchange} />
 
-      <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
+      {/* <div className={styles.content}>
+        <Skeleton loading={loading} active>
+          {!loading && !isEmpty(products) && (
+            <Table dataSource={products} columns={columns} />
+          )}
+        </Skeleton>
+      </div> */}
     </div>
-  )
+  );
 }
